011ff088ba214fcccf90f0c6c092f15c276e9fd9
ryan7984 <ryan7984@gmail.com>
2023-01-14 20:17:42 +0000

    Add header, footer, and styles for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,29 +3,28 @@ const choices = ['rock','paper','scissors'];
 const buttons= document.querySelectorAll('button');
 const prompt = document.querySelector('#prompt');
 
-        let playerChoice, computerChoice, result;
-            buttons.forEach(button => {
-            button.addEventListener('click', () => {
-                //This will be the choice from the player.
-                 playerChoice = button.id;
+let playerChoice, computerChoice, result;
+  buttons.forEach(button => {
+  button.addEventListener('click', () => {
+               
+  //This will be the choice from the player.
+  playerChoice = button.id;
 
-                //This will be the computers choice
-                computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  //This will be the computers choice
+  computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-
-                //This will determine who the winner is between player and computer. 
+  //This will determine who the winner is between player and computer. 
         
-                if (playerChoice === computerChoice) {
-                    result = 'tie';
-                } else if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
-                    (playerChoice === 'paper' && computerChoice === 'rock') ||
-                    (playerChoice === 'scissors' && computerChoice === 'paper')) {
-                    result = 'player';
-                } else {
-                    result = 'computer';
+  if (playerChoice === computerChoice) {
+    result = 'tie';
+      } else if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
+                (playerChoice === 'paper' && computerChoice === 'rock') ||
+                (playerChoice === 'scissors' && computerChoice === 'paper')) {
+                result = 'player';
+        } else  {
+                result = 'computer';
                 }
-                prompt.textContent = `You chose ${playerChoice}, the computer chose ${computerChoice}. You ${result === 'player' ? 'win' : result === 'tie' ? 'tie' : 'lose'}.`;
-
+                prompt.textContent = `You chose ${playerChoice}, the computer chose ${computerChoice}. You ${result === 'player' ? 'win' : result === 'tie' ? 'tie' : 'lose'}.`;              
             });
         });
             
