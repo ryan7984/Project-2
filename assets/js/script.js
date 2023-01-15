83d0c039756a69d2e8b82f1976fd848102f7fea9
ryan7984 <ryan7984@gmail.com>
2023-01-15 14:21:22 +0000

    tidy and add additional comments to htlm, css and javascript files.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,40 +6,41 @@ let playerScore = 0;
 let computerScore = 0;
 const scoreboard = document.querySelector('#scoreboard');
 
+//Click event listner 
 let playerChoice, computerChoice, result;
   buttons.forEach(button => {
   button.addEventListener('click', () => {
                
-  //This will be the choice from the player.
+  //The players choice.
   playerChoice = button.id;
 
-  //This will be the computers choice
+  //The computers choice.
   computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-  //This will determine who the winner is between player and computer. 
-        
+  //This will decide the winner between the player and computer and display the result in the prompt.  
   if (playerChoice === computerChoice) {
     result = 'tie';
-      } else if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
-                (playerChoice === 'paper' && computerChoice === 'rock') ||
-                (playerChoice === 'scissors' && computerChoice === 'paper')) {
-                result = 'player';
-        } else  {
-                result = 'computer';
-                }
-
-               prompt.textContent = `You chose ${playerChoice}, the computer chose ${computerChoice}. You ${result === 'player' ? 'win' : result === 'tie' ? 'tie' : 'lose'}.`; 
-                
-                if (result === 'player'){
-                playerScore++;
+    } else if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
+              (playerChoice === 'paper' && computerChoice === 'rock') ||
+              (playerChoice === 'scissors' && computerChoice === 'paper')) {
+              result = 'player';
+      } else  {
+              result = 'computer';
+              }
+              prompt.textContent = `You chose ${playerChoice}, the computer chose ${computerChoice}. You ${result === 'player' ? 'win' : result === 'tie' ? 'tie' : 'lose'}.`; 
+            
+            //This will determine the score.
+            if (result === 'player'){
+              playerScore++;
             } else if (result === 'computer'){
-                computerScore++;
+              computerScore++;
             }
 
+            //This will display the score, player score on the left and computer score on right of the screen.
             document.querySelector('#game-left').textContent= `Player Score: ${playerScore}`;
             document.querySelector('#game-right').textContent= `Computer Score: ${computerScore}`;
-            });
-        });
+      });
+   });
             
 
         
