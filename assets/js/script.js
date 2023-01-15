ecf260f5507c3c3f1e9efb1a8bf05527f4f7b001
ryan7984 <ryan7984@gmail.com>
2023-01-15 15:39:37 +0000

    fix jshint error for prompt redefinition change promt to promptText.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 
+/*jshint esversion: 6 */
 const choices = ['rock','paper','scissors'];
 const buttons= document.querySelectorAll('button');
-const prompt = document.querySelector('#prompt');
+const promptText = document.querySelector('#prompt');
 let playerScore = 0;
 let computerScore = 0;
-const scoreboard = document.querySelector('#scoreboard');
 
 //Click event listner 
 let playerChoice, computerChoice, result;
@@ -27,7 +27,7 @@ let playerChoice, computerChoice, result;
       } else  {
               result = 'computer';
               }
-              prompt.textContent = `You chose ${playerChoice}, the computer chose ${computerChoice}. You ${result === 'player' ? 'win' : result === 'tie' ? 'tie' : 'lose'}.`; 
+              promptText.textContent = `You chose ${playerChoice}, the computer chose ${computerChoice}. You ${result === 'player' ? 'win' : result === 'tie' ? 'tie' : 'lose'}.`; 
             
             //This will determine the score.
             if (result === 'player'){
